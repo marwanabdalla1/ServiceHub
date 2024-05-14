06b6ce8cfa083f7a0c7789c2bed2ad247d5dae49
“Marwan” <ge27zec@mytum.de>
2024-05-14 22:22:16 +0200

    feat: use vanilla react with tailwind

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-6">
+      <header className="bg-blue-500 text-white p-6 rounded-lg shadow-md">
+      <h1 className="text-3xl font-bold">Welcome to React with Tailwind CSS</h1>
+      </header>      
+    </div>
+  );
+}
+
+export default App;
