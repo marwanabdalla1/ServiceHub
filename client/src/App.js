5e9b99b353625f2d1169e53ea76b0a0cf56f3b69
“Marwan” <ge27zec@mytum.de>
2024-05-14 23:18:37 +0200

    feat: Add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import NavigationBar from './components/Navbar';
 function App() {
   return (
     <div className="text-center p-6">
+      <NavigationBar/>
       <header className="bg-blue-500 text-white p-6 rounded-lg shadow-md">
       <h1 className="text-3xl font-bold">Welcome to React with Tailwind CSS</h1>
       </header>      
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,14 @@
+const {nextui} = require('@nextui-org/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 }
 
 
