5271bbbc4245db0e8239b6c465cb2e8d4c8150fb
Yujuan Sun <yujuan.sun@tum.de>
2024-05-23 11:34:15 +0800

    Add the sign-in and sign-up page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
 import FilterPage from './Pages/FilterPage';
 import ProfilePage from './Pages/ProfilePage';
+import SignInPage from './Pages/SignInPage';
+import SignUpPage from './Pages/SignUpPage';
 import NavigationBar from './components/Navbar';
-function App() {
 
-    return <BrowserRouter>
-          <NavigationBar />
-      <Routes>
-          <Route path="/" element={<FilterPage/>} />   
-          <Route path="/profile" element={<ProfilePage/>} />
-      </Routes>
-      </BrowserRouter>
-    // <FilterPage/>
-  
+function MainRoutes() {
+    const location = useLocation();
+    const showNavBar = location.pathname !== "/signin" && location.pathname !== "/signup";
+
+    return (
+        <>
+            {showNavBar && <NavigationBar/>}
+            <Routes>
+                <Route path="/signin" element={<SignInPage/>}/>
+                <Route path="/signup" element={<SignUpPage/>}/>
+                <Route path="/" element={<FilterPage/>}/>
+                <Route path="/profile" element={<ProfilePage/>}/>
+            </Routes>
+        </>
+    );
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <MainRoutes/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
