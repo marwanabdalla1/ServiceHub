4cb406f1852bf6e79412fafdd04ef1bacda621b1
Yujuan Sun <yujuan.sun@tum.de>
2024-05-25 02:06:14 +0800

    improve icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,32 +2,32 @@ import React from 'react';
 import {BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
 import FilterPage from './Pages/FilterPage';
 import ProfilePage from './Pages/ProfilePage';
-import SignInPage from './Pages/SignInPage';
+import SignInPage from './Pages/LoginPage';
 import SignUpPage from './Pages/SignUpPage';
 import NavigationBar from './components/Navbar';
-
-function MainRoutes() {
-    const location = useLocation();
-    const showNavBar = location.pathname !== "/signin" && location.pathname !== "/signup";
-
-    return (
-        <>
-            {showNavBar && <NavigationBar/>}
-            <Routes>
-                <Route path="/signin" element={<SignInPage/>}/>
-                <Route path="/signup" element={<SignUpPage/>}/>
-                <Route path="/" element={<FilterPage/>}/>
-                <Route path="/profile" element={<ProfilePage/>}/>
-            </Routes>
-        </>
-    );
-}
 
 function App() {
     return (
         <BrowserRouter>
             <MainRoutes/>
         </BrowserRouter>
+    );
+}
+
+function MainRoutes() {
+    const location = useLocation();
+    const showNavBar = location.pathname !== "/login" && location.pathname !== "/signup";
+
+    return (
+        <>
+            {showNavBar && <NavigationBar/>}
+            <Routes>
+                <Route path="/login" element={<SignInPage/>}/>
+                <Route path="/signup" element={<SignUpPage/>}/>
+                <Route path="/" element={<FilterPage/>}/>
+                <Route path="/profile" element={<ProfilePage/>}/>
+            </Routes>
+        </>
     );
 }
 
