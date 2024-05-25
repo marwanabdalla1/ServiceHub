8e3d2fe483c47225fbc4bb2c0711e51c7ff735bf
Weixin Christina Yan <christinayan2001@gmail.com>
2024-05-25 13:03:52 +0200

    booking process preliminarily done (a lot of hard-coded stuff). To be refined later

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,31 @@ import { BrowserRouter,Routes, Route } from "react-router-dom";
 import FilterPage from './Pages/FilterPage';
 import ProfilePage from './Pages/ProfilePage';
 import NavigationBar from './components/Navbar';
+import SelectTimeslot from './Pages/bookingSteps/SelectTimeslotPage'
+import {BookingProvider} from "./context/BookingContext";
+import UpdateProfile from "./Pages/bookingSteps/UpdateProfile";
+import ReviewAndConfirm from "./Pages/bookingSteps/ReviewAndConfirm";
+import CreateAccountOrSignIn from "./Pages/bookingSteps/CreateAccountOrSignIn";
 function App() {
 
-    return <BrowserRouter>
+    return (
+        <BookingProvider>
+            <BrowserRouter>
           <NavigationBar />
+
       <Routes>
           <Route path="/" element={<FilterPage/>} />   
           <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/select-timeslot" element={<SelectTimeslot/>} />
+          <Route path="/create-account-or-sign-in" element={<CreateAccountOrSignIn />} />
+          <Route path="/update-profile" element={<UpdateProfile />} />
+          <Route path="/review-and-confirm" element={<ReviewAndConfirm />} />
+
       </Routes>
       </BrowserRouter>
+
+        </BookingProvider>
+    )
     // <FilterPage/>
   
 }
