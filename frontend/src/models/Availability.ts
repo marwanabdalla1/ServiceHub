ee88cdc45eb64d43ae29732b1a0806af43c8f639
“Marwan” <ge27zec@mytum.de>
2024-06-07 17:23:28 +0200

    fix: Availability

--- frontend/src/models/Availability.ts
+++ frontend/src/models/Availability.ts
@@ -1,8 +1,6 @@
 // import {AppointmentModel} from "@devexpress/dx-react-scheduler";
 // Import your custom Appointment class and the necessary data.
 import { Appointment, appointments as importedAppointments } from './AppointmentModel';
-import { User } from "./Account";
-import { DaysOfWeek } from "./enums";
 import { Timeslot } from "./Timeslot";
 
 
@@ -14,33 +12,31 @@ import { Timeslot } from "./Timeslot";
 // }
 
 export class Availability {
-    dayOfWeek: DaysOfWeek;
     timeslots: Timeslot[];
-    constructor(dayOfWeek: DaysOfWeek, timeslots: Timeslot[]) {
-        this.dayOfWeek = dayOfWeek;
+    constructor(timeslots: Timeslot[]) {
         this.timeslots = timeslots;
     }
 };
 
-export interface SchedulerAppointment {
-    id: number;
-    startDate: Date;
-    endDate: Date;
-    title: string;
-    type?: string;
-    user?: User;
-    service?: string;
-    bookingRef?: string;
-}
+// export interface SchedulerAppointment {
+//     id: number;
+//     startDate: Date;
+//     endDate: Date;
+//     title: string;
+//     type?: string;
+//     user?: User;
+//     service?: string;
+//     bookingRef?: string;
+// }
 
-export const mapAppointmentsToScheduler = (appointments: Appointment[]): SchedulerAppointment[] => {
-    return appointments.map((appointment, index) => ({
-        id: index,
-        startDate: new Date(appointment.date), // Assuming appointment.date is the start date
-        endDate: new Date(new Date(appointment.date).getTime() + 60 * 60 * 1000), // adding 1 hour by default
-        title: appointment.service,
-        user: appointment.user,
-        service: appointment.service,
-        bookingRef: appointment.bookingRef
-    }));
-};
+// export const mapAppointmentsToScheduler = (appointments: Appointment[]): SchedulerAppointment[] => {
+//     return appointments.map((appointment, index) => ({
+//         id: index,
+//         startDate: new Date(appointment.date), // Assuming appointment.date is the start date
+//         endDate: new Date(new Date(appointment.date).getTime() + 60 * 60 * 1000), // adding 1 hour by default
+//         title: appointment.service,
+//         user: appointment.user,
+//         service: appointment.service,
+//         bookingRef: appointment.bookingRef
+//     }));
+// };
