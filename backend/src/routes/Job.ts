f8cbed899c1652903e8831ea72ef41f3b85ea6fa
nadinesamy21 <nadine.samy@tum.de>
2024-06-24 17:14:08 +0200

    implemented getter functions for provider and receiver to display name in job card

--- backend/src/routes/Job.ts
+++ backend/src/routes/Job.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createJob, getJobsByProvider } from "../controller/JobController";
-import {authenticate} from "../middleware/authenticate";
+import { authenticate } from "../middleware/authenticate";
 
 
 const router = express.Router();
