d0014a68b8c24fb3580f74c36e1506055f554402
“Marwan” <ge27zec@mytum.de>
2024-07-18 13:52:54 +0200

    feat: make location name have max width

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -1,4 +1,3 @@
-// MediaCard component
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,9 +7,9 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import {ServiceOffering} from '../models/ServiceOffering';
-import {GoStarFill} from "react-icons/go";
-import {Link} from 'react-router-dom';
+import { ServiceOffering } from '../models/ServiceOffering';
+import { GoStarFill } from "react-icons/go";
+import { Link } from 'react-router-dom';
 
 interface MediaCardProps {
     offering: ServiceOffering;
@@ -18,18 +17,18 @@ interface MediaCardProps {
     loading: boolean;
 }
 
-export default function MediaCard({offering, profileImageUrl, loading}: MediaCardProps) {
+export default function MediaCard({ offering, profileImageUrl, loading }: MediaCardProps) {
     const provider = offering.provider;
     return (
         <div className='border margin-4 rounded-2xl overflow-hidden'>
             <Card>
                 {loading ? (
                     <Box display="flex" justifyContent="center" alignItems="center" height={280}>
-                        <CircularProgress/>
+                        <CircularProgress />
                     </Box>
                 ) : (
                     <CardMedia
-                        sx={{height: 280}}
+                        sx={{ height: 280 }}
                         image={profileImageUrl ? profileImageUrl : provider.profileImageId}
                         title="Service Image"
                     />
@@ -42,7 +41,7 @@ export default function MediaCard({offering, profileImageUrl, loading}: MediaCar
                                 <Typography gutterBottom variant="h5" component="div">
                                     {offering.serviceType}
                                 </Typography>
-                                <Typography variant="h6" color="text.secondary" sx={{mt: -1}}>
+                                <Typography variant="h6" color="text.secondary" sx={{ mt: -1 }}>
                                     {provider.firstName} {provider.lastName}
                                 </Typography>
                             </div>
@@ -51,7 +50,12 @@ export default function MediaCard({offering, profileImageUrl, loading}: MediaCar
                                 <Typography variant="h6" color="text.secondary">
                                     €{offering.hourlyRate}/hr
                                 </Typography>
-                                <Typography variant="body1" color="text.secondary">
+                                <Typography variant="body1" color="text.secondary" sx={{
+                                    maxWidth: '150px', // Set max width to prevent overflow
+                                    whiteSpace: 'nowrap', // Prevent text from wrapping
+                                    overflow: 'hidden', // Hide overflow text
+                                    textOverflow: 'ellipsis' // Add ellipsis if text overflows
+                                }}>
                                     {provider.location}
                                 </Typography>
                             </div>
@@ -69,23 +73,23 @@ export default function MediaCard({offering, profileImageUrl, loading}: MediaCar
                                         <Typography variant="body2" color="text.secondary">
                                             {offering.rating.toFixed(2)}
                                         </Typography>
-                                        <GoStarFill className='text-yellow-500'/>
+                                        <GoStarFill className='text-yellow-500' />
                                     </>)}
                             </div>
                             {offering.isCertified && (
                                 <Typography className='text-green-500' variant="body2"
-                                            sx={{
-                                                fontWeight: 'bold'
-                                            }}>
+                                    sx={{
+                                        fontWeight: 'bold'
+                                    }}>
                                     Licensed
                                 </Typography>
                             )}
                             {provider.isPremium && (
                                 <Typography className='font-extrabold' variant="body2"
-                                            sx={{
-                                                color: "#5e63b6",
-                                                fontWeight: 'bold'
-                                            }}>
+                                    sx={{
+                                        color: "#5e63b6",
+                                        fontWeight: 'bold'
+                                    }}>
                                     Promoted
                                 </Typography>
                             )}
